3cf424263c153221c348bf80af433b0adc3901b3
Fabio Colombini <fabiorcolombini@gmail.com>
2025-10-03 20:31:15 -0300

    aula6

--- src/Aula_6/Main_Pessoa.java
+++ src/Aula_6/Main_Pessoa.java
@@ -1,9 +1,52 @@
 package Aula_6;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main_Pessoa {
     public static void main(String[] args) {
-        Pessoa p = new Pessoa("Fabio", "234234");
-        p.imprimir();
+        ArrayList<Pessoa> lstPessoas = new ArrayList<>();
         
+        /*Pessoa p1 = new Pessoa("Joao", "1111");
+        Pessoa p2 = new Pessoa("Maria", "22222");
+        Pessoa p3 =  new Pessoa("Carla", "33333");
+        
+        lstPessoas.add(p1);
+        lstPessoas.add(p2);
+        lstPessoas.add(p3);*/
+        
+        // mostrem o nome da 2º pessoa da lista (Maria)
+        //System.out.println(lstPessoas.get(1).getNome());
+        
+        // mostre todos os dados da 1º pessoa (imprimir)
+        //lstPessoas.get(0).imprimir();
+        
+        Scanner sc = new Scanner(System.in);
+        Pessoa p = null;
+        int count = 0;
+        
+        // cadastro
+        do {
+            System.out.print("\nNome: ");
+            String nome = sc.next();
+            System.out.print("\nCPF: ");
+            String cpf = sc.next();
+
+            p = new Pessoa(nome, cpf);
+            lstPessoas.add(p);
+            count++;
+        } while (count < 3);
+        
+        // imprimir todos
+        /*for (Pessoa pe : lstPessoas) {
+            pe.imprimir();
+        }*/
+        
+        // Busca por nome
+        for (Pessoa pessoa : lstPessoas) {
+            if (pessoa.getNome().equals("Fabio")) {
+                pessoa.imprimir();
+            }
+        }
     }
 }
